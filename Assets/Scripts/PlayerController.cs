3c0202cf85d7b34b6367906b23862be8520e6961
RIZWAN <morizwan1410@gmail.com>
2025-12-07 23:50:40 +0530

    Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float xClamp = 3f;
+    [SerializeField] float zClamp = 2f;
+
+    Vector2 movement;
+    Rigidbody rigidBody;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+    public void Move(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+    }
+
+    private void HandleMovement()
+    {
+        Vector3 currentPosition = rigidBody.position;
+        Vector3 moveDirection = new Vector3(movement.x, 0f, movement.y);
+        Vector3 newPosition = currentPosition + moveDirection * (moveSpeed * Time.fixedDeltaTime);
+
+        newPosition.x = Mathf.Clamp(newPosition.x, -xClamp, xClamp);
+        newPosition.z = Mathf.Clamp(newPosition.z, -zClamp, zClamp);
+
+        rigidBody.MovePosition(newPosition);
+    }
+}
