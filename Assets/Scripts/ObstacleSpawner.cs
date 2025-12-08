6a12f2d39e89852b9d56e6cab79a671e019624a8
RIZWAN <morizwan1410@gmail.com>
2025-12-09 00:33:06 +0530

    multiple obstacles

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject obstaclePrefab;
+    [SerializeField] GameObject[] obstaclePrefabs;
     [SerializeField] float obstacleSpawnTime = 1f;
+    [SerializeField] Transform obstacleParent;
+    [SerializeField] float spawnWidth = 4f;
 
     void Start()
     {
@@ -15,8 +17,10 @@ public class ObstacleSpawner : MonoBehaviour
     {
         while(true )
         {
+            GameObject obstaclePrefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Length)];
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnWidth,spawnWidth), transform.position.y, transform.position.z);
             yield return new WaitForSeconds(obstacleSpawnTime);
-            Instantiate(obstaclePrefab, transform.position, Random.rotation);
+            Instantiate(obstaclePrefab, spawnPos, Random.rotation, obstacleParent);
         }
     }
 
