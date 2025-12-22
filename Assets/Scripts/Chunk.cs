a6857df274164e571b65ec0aa917633cb5ee775e
RIZWAN <morizwan1410@gmail.com>
2025-12-23 00:18:16 +0530

    more obstacles update

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Chunk : MonoBehaviour
@@ -12,8 +13,19 @@ public class Chunk : MonoBehaviour
 
     void SpawnFence()
     {
-        int randomLaneIndex = Random.Range(0, lanes.Length);
-        Vector3 spawnPosition = new Vector3(lanes[randomLaneIndex], transform.position.y, transform.position.z);
-        Instantiate(fencePrefab, spawnPosition, Quaternion.identity, this.transform);
+        List<int> availableLanes = new List<int> { 0, 1, 2 };
+        int fencesToSpawn = Random.Range(0, 3);
+
+        for (int i = 0; i < fencesToSpawn; i++)
+        {
+            if (availableLanes.Count <= 0) break;
+
+            int randomLaneIndex = Random.Range(0, availableLanes.Count);
+            int selectedLane = availableLanes[randomLaneIndex];
+            availableLanes.RemoveAt(randomLaneIndex);
+
+            Vector3 spawnPosition = new Vector3(lanes[randomLaneIndex], transform.position.y, transform.position.z);
+            Instantiate(fencePrefab, spawnPosition, Quaternion.identity, this.transform);         
+        }
     }
 }
