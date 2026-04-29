2ea5ad7b0f8a9405ecc4aa410961e0c8381ba8b2
RIZWAN <morizwan1410@gmail.com>
2026-04-29 22:36:12 +0530

    Hit animation cooldown

--- Assets/Scripts/Player/PlayerCollisionHandler.cs
+++ Assets/Scripts/Player/PlayerCollisionHandler.cs
@@ -3,10 +3,23 @@ using UnityEngine;
 public class PlayerCollisionHandler : MonoBehaviour
 {
     [SerializeField] Animator animator;
+    [SerializeField] float collisionCoolDown = 1f;
 
     const string hitString = "Hit";
+
+    float coolDownTimer = 0f;
+
+    void Update()
+    {
+        coolDownTimer += Time.deltaTime;
+    }
     void OnCollisionEnter(Collision other)
     {
+        if (coolDownTimer < collisionCoolDown)
+        {
+            return;
+        }
         animator.SetTrigger(hitString);
+        coolDownTimer = 0f;
     }
 }
