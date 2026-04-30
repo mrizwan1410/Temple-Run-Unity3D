00642798fbef86676f85561a8532dffd4c5f79a7
RIZWAN <morizwan1410@gmail.com>
2026-04-30 18:44:54 +0530

    speed up after pickups

--- Assets/Scripts/Pickups/Apple.cs
+++ Assets/Scripts/Pickups/Apple.cs
@@ -2,8 +2,16 @@ using UnityEngine;
 
 public class Apple : Pickup
 {
+    [SerializeField] float adjustChangeMoveSpeedAmount = 3f;
+
+    LevelGenerator levelGenerator;
+
+    private void Start()
+    {
+        levelGenerator = FindFirstObjectByType<LevelGenerator>();
+    }
     protected override void OnPickup()
     {
-        Debug.Log("Power Up!");
+        levelGenerator.ChangeChunkMoveSpeed(adjustChangeMoveSpeedAmount);
     }
 }
